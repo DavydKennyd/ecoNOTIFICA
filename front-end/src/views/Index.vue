e926a0bb43983a8ab4b590a6d7614c1b700922cc
Jainy Eduarda <jainyedu16@gmail.com>
2025-02-14 11:45:19 -0300

    Modifiquei a página inicial, sua foto e algumas configurações, porém vou continuar depois

--- front-end/src/views/Index.vue
+++ front-end/src/views/Index.vue
@@ -4,7 +4,7 @@
 
     <section class="conteine">
 
-        <img :src="require('@/assets/mundo do Index.jpg')" alt="imagem de fundo" class="imagem_de_fundo">
+        <img :src="require('@/assets/image_fundo.jpg')" alt="imagem de fundo" class="imagem_de_fundo">
 
         <div class="DeCima inria">
             <center>
@@ -22,31 +22,31 @@
 
     <section class="rsIni">
 
-  <hr style="  width: 100vw; color: var(--colorBanco); height: 5px; margin-left: -10px;">
+  <hr class="divisoria">
         <div class="rs">
             <div class="R">
                 <p class="title">REPENÇAR</p>
-                <p class="descricao">Oque eu posso fazer para ajudar?</p>
+                <p class="descricao">O que eu posso fazer para ajudar?</p>
             </div>
 
             <div class="R">
                 <p class="title">RECUSAR</p>
-                <p class="descricao">É necessario comprar tudo o que compro? Tudo o que compramos gera residuos</p>
+                <p class="descricao">É necessário comprar tudo o que compro? Tudo o que compramos gera resíduos</p>
             </div>
 
             <div class="R">
                 <p class="title">REDUZIR</p>
-                <p class="descricao">Eu realmente precisso isso?</p>
+                <p class="descricao">Eu realmente precisso disso?</p>
             </div>
 
             <div class="R">
                 <p class="title">REUTILIZAR</p>
-                <p class="descricao">Essa garrafa precisa ir pro lixo? sera que eu não posso usar para algo?</p>
+                <p class="descricao">Essa garrafa precisa ir para o lixo? Sára que eu não posso usar para algo?</p>
             </div>
 
             <div class="R">
                 <p class="title">RECICLAR</p>
-                <p class="descricao">Entre na EcoNotífica e vem reciclar</p>
+                <p class="descricao">Entre no EcoNotifica e vem reciclar!</p>
             </div>
         </div>
 
@@ -89,11 +89,14 @@ body {
 
 .imagem_de_fundo {
     width: 100vw;
+    height: 100vh;
+    object-fit: cover;
+    display: block;
 }
+
 
 .conteine {
     margin-top: 0;
-
     position: absolute;
     z-index: -2;
 
@@ -103,10 +106,10 @@ body {
     position: absolute;
     top: 0;
     width: 100vw;
-    height: 120vh;
+    height: 100vh;
     z-index: 0;
     background: rgb(0, 0, 0);
-    background: linear-gradient(166deg, rgba(0, 0, 0, 0.44) 0%, rgba(0, 0, 0, 1) 62%);
+    background: linear-gradient(166deg, rgba(0, 0, 0, 0.3) 0%, rgba(0, 0, 0, 0.7) 60%);
 }
 
 h1 {
@@ -115,6 +118,13 @@ h1 {
     font-size: 5.5vw;
 }
 
+.divisoria {
+    width: 100%;
+    height: 5px;
+    background-color: var(--colorBanco);
+    border: none;
+    margin: 20px 0;
+}
 
 
 
@@ -122,11 +132,15 @@ h1 {
 .rsIni{
     margin-top: 100vh;
     color: white;
+    text-align: center;
 }
 
 .rs {
     display: flex;
     justify-content: space-between;
+    flex-wrap: wrap;
+    gap: 10px;
+    padding: 5px;
 }
 
 .R {
@@ -137,7 +151,7 @@ h1 {
     width: calc(60vw / 5);
     border: 2px white solid;
     border-radius: 20px;
-    /* background-color: rgba(255, 255, 255, 0.308); */
+    background-color: rgba(255, 255, 255, 0.1);
 }
 
 .title {
