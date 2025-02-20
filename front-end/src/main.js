47a07188600031dec42925bdfcb9f2de2aca45b3
root <root@debian.ifrn.local>
2025-02-20 16:07:46 -0300

    atualizando coisas

--- front-end/src/main.js
+++ front-end/src/main.js
@@ -4,6 +4,14 @@ import router from './router';
 import '@/assets/css/global.css'
 
 
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faWhatsapp, faInstagram } from '@fortawesome/free-brands-svg-icons'
+
+
+library.add(faWhatsapp, faInstagram)
+
 createApp(App)
-  .use(router)  // Não se esqueça de adicionar o Vue Router
+  .use(router)  
+  .component('font-awesome-icon', FontAwesomeIcon) 
   .mount('#app');
--- front-end/src/views/Index.vue
+++ front-end/src/views/Index.vue
@@ -22,7 +22,7 @@
 
     <section class="rsIni">
 
-  <hr class="divisoria">
+        <hr class="divisoria">
         <div class="rs">
             <div class="R">
                 <p class="title">REPENÇAR</p>
@@ -54,7 +54,7 @@
 
     </section>
 
-<!-- 
+    <!-- 
     <section class="contabilização">
 
         <div>
@@ -75,13 +75,95 @@
     <section class="mapa">
 
 
+
     </section>
+
+    <footer>
+
+        <center>
+            <ul class="footer_ul">
+                <li class="footer_ul_li">
+                    <a href=""><font-awesome-icon :icon="['fab', 'whatsapp']" class="ico" /></a>
+                </li>
+                <li class="footer_ul_li">
+                    <a href=""><font-awesome-icon :icon="['fab', 'instagram']" class="ico" /></a>
+                </li>
+
+            </ul>
+            <p class="developer">Made by developer the IFRN</p>
+            <ul style="display: flex;">
+                <li><a href="https://raque-lvi.github.io/Curriculo_onlin/">
+                        <p id="footer_i" style="font-size: 20px;"> Web </p>
+                    </a></li>
+                <li> <a href="https://github.com/Raque-lVi"><i id="footer_i" class="fa-brands fa-github"></i></a></li>
+            </ul>
+
+        </center>
+
+    </footer>
 
 
 
 </template>
 
 <style>
+
+footer {
+
+    background-color: rgb(0, 46, 15);
+}
+
+.ico{
+    font-size: 1.5vw;
+}
+footer ul{
+    display: flex;
+    right: 0;
+    width: 5%;
+}
+/* .footer_ul {
+    display: flex;
+    padding-top: 2%;
+
+} */
+
+/* footer ul {
+    margin-right: 45%;
+    margin-left: 45%;
+
+}
+
+footer ul li {
+    margin: 10%;
+
+}
+
+#footer_i {
+
+    font-size: 30px;
+    color: var(--cor_basic);
+}
+
+#footer_i:hover {
+
+    font-size: 30px;
+    color: green;
+
+}
+
+.developer {
+    padding-top: 2%;
+}
+
+.developer:hover {
+    color: var(--cor_principal);
+} */
+
+
+
+
+
+
 body {
     overflow-x: hidden;
     background: rgb(0, 0, 0);
@@ -129,7 +211,7 @@ h1 {
 
 
 
-.rsIni{
+.rsIni {
     margin-top: 100vh;
     color: white;
     text-align: center;
@@ -141,8 +223,10 @@ h1 {
     flex-wrap: wrap;
     gap: 10px;
     padding: 5px;
-    margin-top: 0;
-    background-color: rgb(0, 0, 0);
+    margin-top: -2.5vh;
+    padding-bottom: 3vh;
+    backdrop-filter: blur(10px);
+    background-color: rgba(0, 0, 0, 0.151);
 }
 
 .R {
@@ -180,7 +264,17 @@ h1 {
 
 
 <script>
+
 import Cabecalho from "@/components/cabecario.vue";
+
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { faWhatsapp, faInstagram } from '@fortawesome/free-brands-svg-icons'
+
+
+library.add(faWhatsapp, faInstagram)
+
+
+
 
 export default {
     name: "index_eco",
