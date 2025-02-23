2f780cac2d9fcd6f7df286491dd45ad9e462ddc5
welson-rodrigues <welson.rosendo@escolar.ifrn.eu.br>
2025-02-23 18:33:32 -0300

    Voltando para o banco de dados online

--- back-end/config/db.js
+++ back-end/config/db.js
@@ -2,9 +2,9 @@ const { Pool } = require('pg');
 
 const pool = new Pool({
   user: 'postgres',               // Seu nome de usuário do banco
-  host: 'localhost',  // Endereço do servidor PostgreSQL
-  database: 'ecoNOTIFICA',           // Nome do banco de dados
-  password: 'ifrn.cn',  // Sua senha
+  host: 'maliciously-leisurely-hound.data-1.use1.tembo.io',  // Endereço do servidor PostgreSQL
+  database: 'postgres',           // Nome do banco de dados
+  password: 'aWHgMM4O0SaKmZnp',  // Sua senha
   port: 5432,
   ssl: {
     rejectUnauthorized: false,
